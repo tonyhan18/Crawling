caf2872390b7ca614a9bcc39321f50fad8f44992
tonyhan18 <ourclub7279@gmail.com>
2025-02-10 23:21:52 +0900

    250210 updated - 크롤링 사이트 준비중

---
+++ PriceMap/Interfaces/IDatabase.cs
@@ -0,0 +1,17 @@
+﻿using PriceMap.Models;
+
+namespace PriceMap.Interfaces
+{
+    public interface IDatabase<T>
+    {
+        Task<List<T>> GetAsync();
+
+        T GetDetail(int? id);
+
+        void Create(T entity);
+
+        void Update(int? id, T entity);
+
+        void Delete(int? id);
+    }
+}
---
+++ PriceMap/Python/PythonRunner.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace PriceMap.Python
+{
+    public class PythonRunner
+    {
+        private readonly ProcessStartInfo _psi;
+
+        public PythonRunner()
+        {
+            _psi = new ProcessStartInfo
+            {
+                FileName = "python",
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+        }
+
+        public async Task<string> RunScriptAsync(string scriptPath, string args = "")
+        {
+            _psi.Arguments = $"{scriptPath} {args}";
+
+            using (Process process = new Process { StartInfo = _psi })
+            {
+                process.Start();
+
+                string output = await process.StandardOutput.ReadToEndAsync();
+                string error = await process.StandardError.ReadToEndAsync();
+
+                await process.WaitForExitAsync();
+
+                if (process.ExitCode != 0)
+                {
+                    throw new Exception($"Python script exited with error: {error}");
+                }
+
+                return output;
+            }
+        }
+    }
+}
